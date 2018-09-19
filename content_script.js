c0efd4337b58dc48730a52ebb00030ddd4395c0e
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2018-09-19 11:07:42 -0400

    add keyboard shortcuts; handle voters w/o phones

--- content_script.js
+++ content_script.js
@@ -1,14 +1,23 @@
 window.onload = function() {
-  var formattedPhone = document.getElementById('current-number').firstElementChild.innerText;
-  var phone = formattedPhone.replace(/\D/g,'');
+  var numberElement = document.getElementById('current-number');
+  if (numberElement) { // make sure they have a phone number
+    var formattedPhone = numberElement.firstElementChild.innerText;
+    var phone = formattedPhone.replace(/\D/g,'');
+  }
 
   chrome.runtime.sendMessage('getAskToCall', function(askToCall) {
-    if (askToCall) {
+    if (askToCall && numberElement) {
       var dial = confirm('Dial ' + formattedPhone + '?');
       if (dial) {
         chrome.runtime.sendMessage(phone);
       }
     }
+  });
+
+  var reached = true;
+  var switchButton = document.getElementById('switch');
+  switchButton.addEventListener('click', function(event) {
+    reached = !reached;
   });
 
   document.addEventListener('keypress', (event) => {
@@ -23,9 +32,58 @@ window.onload = function() {
       chrome.runtime.sendMessage("hangup");
       return;
     }
-    if (keyName === 'c') {
+    if (keyName === 'c' && numberElement) {
       chrome.runtime.sendMessage(phone);
       return;
+    }
+
+    if (keyName === 'o') {
+      var next = document.querySelectorAll('textarea')[0];
+      next.focus();
+      return;
+    }
+
+    if (keyName === 'Enter') {
+      var nextContact = document.querySelectorAll('input[value="Save & Next Call"]')[0];
+      var nextNumber = document.querySelectorAll('input[value*="Try Number"]')[0];
+      if (!nextContact.classList.contains('ng-hide')) {
+        nextContact.click();
+      } else {
+        nextNumber.click();
+      }
+      return;
+    }
+
+    if (keyName === 's') {
+      var next = document.querySelectorAll('input[value="Skip"]')[0];
+      next.click();
+      return;
+    }
+
+    if (keyName === 'e') {
+      var next = document.querySelectorAll('input[value="Schedule For Events"]')[0];
+      if (next) {
+        next.click();
+      }
+      return;
+    }
+
+    var status = {
+      n: 1,
+      r: 2,
+      m: 5,
+      b: 18,
+      w: 20,
+      d: 25,
+      l: 31
+    }[keyName];
+
+    if (status) {
+      if (reached) {
+        switchButton.click();
+      }
+      var element = document.querySelectorAll('input[name="resultCodeId"][value="' + status + '"]')[0];
+      element.click();
     }
   });
 }
