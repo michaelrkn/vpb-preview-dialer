0be56cc2e1872d0aae7c7260a5fd333c4161ccea
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2018-09-21 14:54:44 -0500

    add authentication

--- background.js
+++ background.js
@@ -8,20 +8,28 @@ chrome.runtime.onInstalled.addListener((details) => {
   }
 });
 
+chrome.runtime.onStartup.addListener((details) => {
+  getToken();
+});
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (message === 'getCallOnLoad') {
     sendResponse(JSON.parse(localStorage.getItem('callOnLoad')));
   } else if (message === "hangup") {
     hangup();
+  } else if (message === 'getToken') {
+    getToken();
   } else {
     checkConnection().then(() => { dial(message) });
   }
 });
 
-window.onload = getToken();
-
 function getToken() {
-  return fetch('https://cardinal-moose-3646.twil.io/capability-token').then(function(response) {
+  var campaignCode = localStorage.getItem('campaignCode');
+  var accessCode = localStorage.getItem('accessCode');
+
+  return fetch('https://' + campaignCode + '.twil.io/capability-token?accessCode=' + accessCode)
+  .then(function(response) {
     return response.json();
   })
   .then(function(json) {
--- options.js
+++ options.js
@@ -1,25 +1,45 @@
 window.onload = function() {
+  document.getElementById('campaign-code').value = this.localStorage.getItem('campaignCode');
+  document.getElementById('access-code').value = this.localStorage.getItem('accessCode');
   document.getElementById('number').value = localStorage.getItem('outgoingCallerID');
   document.getElementById('callOnLoad').checked = localStorage.getItem('callOnLoad');
 
   navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => stream.getTracks()[0].stop()); // get microphone permissions
 
-  fetch('https://cardinal-moose-3646.twil.io/capability-token').then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    Twilio.Device.setup(json.token);
+  var setupForm = document.getElementById('campaign-setup');
+  setupForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var campaignCode = document.getElementById('campaign-code').value;
+    var accessCode = document.getElementById('access-code').value;
+
+    fetch('https://' + campaignCode + '.twil.io/capability-token?accessCode=' + accessCode)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      alert("You're all set up! Make sure to read the keyboard commands on the right, and review the options below.");
+      localStorage.setItem('campaignCode', campaignCode);
+      localStorage.setItem('accessCode', accessCode);
+      chrome.runtime.sendMessage('getToken');
+    })
+    .catch(function(response) {
+      alert("Sorry, the campaign or access code you entered was incorrect.");
+    });
   });
 
-  var form = document.getElementById('verify-phone');
-  form.addEventListener("submit", function(event) {
+  var phoneForm = document.getElementById('verify-phone');
+  phoneForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
     var phone = document.getElementById('number').value.replace(/\D/g,'');
+    var campaignCode = localStorage.getItem('campaignCode');
+
     if (phone.length !== 10) {
       alert('Please enter a valid 10-digit phone number.');
+    } else if (!campaignCode) {
+      alert('Set your campaign and access codes before setting your caller ID.')
     } else {
-      fetch('https://cardinal-moose-3646.twil.io/verify-caller-id?phone=' + phone)
+      fetch('https://' + campaignCode + '.twil.io/verify-caller-id?phone=' + phone)
       .then(function(response) {
         return response.json();
       })
