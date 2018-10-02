1b5a923796a0ec8e4ce2606c66b328e969e85024
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2018-10-02 08:03:39 -0700

    move instructions from options to popup on vpb load

---
+++ instructions.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  this.alert(`Welcome to the Virtual Phone Bank Preview Dialer!
+When a contact loads, you'll be asked if you want to call them.
+The audio will come through your browser.
+
+If you don't make a call at first, you can make it later by pressing "c", or by clicking the phone number.
+When you are on the phone, you can press "h" to hang up.
+
+For "Couldn't Reach" statuses, press:
+  n: Not Home
+  r: Refused
+  m: Moved
+  b: Busy
+  w: Wrong Number
+  d: Disconnected
+  l: Other Language
+
+You don't need to click "I Couldn't Reach..." - just press the key.
+You can also press "s" to open the Scheduler, and "o" to go to Notes (if available).
+
+Then, press Enter for "Save & Next Call".
+Or, press "s" for "Skip" (if available).`)
+};
