9220efd558b9cc6268045956cd45fddf91e4d0e1
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2018-10-04 14:55:48 -0700

    improve instructions

--- instructions.js
+++ instructions.js
@@ -1,10 +1,13 @@
 window.onload = function() {
   this.alert(`Welcome to the Virtual Phone Bank Preview Dialer!
+
 When a contact loads, you'll be asked if you want to call them.
+You can also call by pressing "c", or clicking the phone number.
 The audio will come through your browser.
 
-If you don't make a call at first, you can make it later by pressing "c", or by clicking the phone number.
 When you are on the phone, you can press "h" to hang up.
+Then, press "Enter" for "Save & Next Call".
+Or, press "s" for "Skip" (if available).
 
 For "Couldn't Reach" statuses, press:
   n: Not Home
@@ -16,8 +19,6 @@ For "Couldn't Reach" statuses, press:
   l: Other Language
 
 You don't need to click "I Couldn't Reach..." - just press the key.
-You can also press "s" to open the Scheduler, and "o" to go to Notes (if available).
 
-Then, press Enter for "Save & Next Call".
-Or, press "s" for "Skip" (if available).`)
+You can also press "s" to open the Scheduler, and "o" to go to Notes (if available).`)
 };
