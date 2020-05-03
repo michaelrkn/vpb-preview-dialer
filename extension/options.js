dd352b486102bddd946c07e8fe70e81feb4ad2ed
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2020-05-03 17:15:19 -0500

    send version and user to sentry

--- extension/options.js
+++ extension/options.js
@@ -1,5 +1,11 @@
 if (chrome.runtime.getManifest().update_url !== undefined) {
-  Sentry.init({ dsn: 'https://ed97abb64b8f40bf969f4c6ad509123c@sentry.io/2650962' });
+  Sentry.init({
+    dsn: 'https://ed97abb64b8f40bf969f4c6ad509123c@sentry.io/2650962',
+    release: '2.0.2'
+  });
+  Sentry.configureScope(function(scope) {
+    scope.setUser({"username": localStorage.getItem('campaignCode')});
+  });
 }
 
 window.onload = () => {
