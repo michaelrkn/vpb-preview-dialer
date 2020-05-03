bbff5e33b8831e95142c40e171cdd60b39509273
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2020-05-03 17:18:15 -0500

    bump version

--- extension/options.js
+++ extension/options.js
@@ -1,7 +1,7 @@
 if (chrome.runtime.getManifest().update_url !== undefined) {
   Sentry.init({
     dsn: 'https://ed97abb64b8f40bf969f4c6ad509123c@sentry.io/2650962',
-    release: '2.0.2'
+    release: '2.0.3'
   });
   Sentry.configureScope(function(scope) {
     scope.setUser({"username": localStorage.getItem('campaignCode')});
